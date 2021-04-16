2d83525602ae8f362098529900b80d0294965c3b
Nyoman Adiwinanda <nyoman.adi16@gmail.com>
2021-04-16 18:39:39 +0700

    add users seeder for testing

---
+++ service/seeders/20210416113323-Users.js
@@ -0,0 +1,44 @@
+'use strict';
+
+const { hashPass } = require('../helpers/bcrypt.js');
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+     */
+
+    await queryInterface.bulkInsert(
+      'Users',
+      [
+        {
+          name: 'nameOfUser',
+          email: 'User@mail.com',
+          password: hashPass('User123'),
+          imageUrl:
+            'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png',
+          balance: 0,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+      ],
+      {}
+    );
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+    await queryInterface.bulkDelete('Users', null, {});
+  },
+};
