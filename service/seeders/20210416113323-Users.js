142b859af8777db6dfa486598c9341de7dd47fa4
Nyoman Adiwinanda <nyoman.adi16@gmail.com>
2021-04-17 19:00:45 +0700

    add Seeder Types and Categories

--- service/seeders/20210416113323-Users.js
+++ service/seeders/20210416113323-Users.js
@@ -24,6 +24,7 @@ module.exports = {
           imageUrl:
             'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png',
           balance: 0,
+          condition: 4,
           createdAt: new Date(),
           updatedAt: new Date(),
         },
