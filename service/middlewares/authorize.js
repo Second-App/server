85ae6b6c6704580bc552c8f1e402d2320bc045fe
Nyoman Adiwinanda <nyoman.adi16@gmail.com>
2021-04-17 10:23:42 +0700

    add: 1) WIshlistController 2)delete category and type route

--- service/middlewares/authorize.js
+++ service/middlewares/authorize.js
@@ -1,4 +1,4 @@
-const { Product, User } = require('../models');
+const { Product, User, Cart, Wishlist } = require('../models');
 
 const authorizeProduct = async (req, res, next) => {
   try {
@@ -67,4 +67,77 @@ const authorizeUser = async (req, res, next) => {
   }
 };
 
-module.exports = { authorizeProduct, authorizeUser };
+const authorizeCart = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const UserId = req.decoded.id;
+
+    const data = await Cart.findOne({
+      where: { id },
+      include: ['User'],
+    });
+
+    if (!data)
+      throw {
+        status: 404,
+        msg: 'Id not found',
+      };
+
+    if (data.UserId !== UserId)
+      throw {
+        status: 401,
+        msg: 'Not authorized',
+      };
+
+    next();
+  } catch (err) {
+    if (err.status === 404) {
+      res.status(404).json({ msg: err.msg });
+    } else if (err.status === 401) {
+      res.status(401).json({ msg: err.msg });
+    } else {
+      res.status(500).json({ msg: 'Internal Server Error' });
+    }
+  }
+};
+
+const authorizeWishlist = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const UserId = req.decoded.id;
+
+    const data = await Wishlist.findOne({
+      where: { id },
+      include: ['User'],
+    });
+
+    if (!data)
+      throw {
+        status: 404,
+        msg: 'Id not found',
+      };
+
+    if (data.UserId !== UserId)
+      throw {
+        status: 401,
+        msg: 'Not authorized',
+      };
+
+    next();
+  } catch (err) {
+    if (err.status === 404) {
+      res.status(404).json({ msg: err.msg });
+    } else if (err.status === 401) {
+      res.status(401).json({ msg: err.msg });
+    } else {
+      res.status(500).json({ msg: 'Internal Server Error' });
+    }
+  }
+};
+
+module.exports = {
+  authorizeProduct,
+  authorizeUser,
+  authorizeWishlist,
+  authorizeCart,
+};
