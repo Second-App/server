45d3fcf36a2430e803aa065f6f2ef0348070f5c8
Nyoman Adiwinanda <nyoman.adi16@gmail.com>
2021-04-18 10:43:45 +0700

    add Socket.io: 1) sendChat 2) updateAuction 3) updateBalance

--- service/app.js
+++ service/app.js
@@ -8,13 +8,30 @@ const errorHandler = require('./middlewares/errorHandler.js');
 const cors = require('cors');
 
 const app = express();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.use(routes);
-
 app.use(errorHandler);
+
+io.on('connection', (socket) => {
+  console.log('socket.io connected');
+
+  socket.on('sendChat', (chatData) => {
+    io.emit('getSendChat', chatData);
+  });
+
+  socket.on('updateAuction', (auctionData) => {
+    io.emit('getAuctionData', auctionData);
+  });
+
+  socket.on('updateBalance', (balanceData) => {
+    socket.emit('getBalanceData', balanceData);
+  });
+});
 
 module.exports = app;
