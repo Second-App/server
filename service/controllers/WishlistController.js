db08cfeb360309ac43ca557e13d23c82fee3a2a9
rbernanda <acappemenang@gmail.com>
2021-04-21 22:19:37 +0700

    jest test Product and wishlist

--- service/controllers/WishlistController.js
+++ service/controllers/WishlistController.js
@@ -1,76 +1,79 @@
-const { Wishlist } = require('../models');
+const { Wishlist } = require('../models')
 
 class WishlistController {
   static async createWishlist(req, res, next) {
     try {
-      const UserId = req.decoded.id;
-      const { ProductId } = req.body;
+      const UserId = req.decoded.id
+      const { ProductId } = req.body
 
       const newWishlist = {
         ProductId,
         UserId,
-      };
+      }
 
       const checkAlreadyExist = await Wishlist.findOne({
         where: {
           ProductId,
           UserId,
         },
-      });
+      })
 
       if (checkAlreadyExist)
         throw {
           name: 'CustomError',
           msg: 'You already wishlist this product',
           status: 400,
-        };
+        }
 
-      const data = await Wishlist.create(newWishlist);
-
-      if (!data) throw err;
-
-      res.status(201).json({
-        msg: 'success add wishlist',
-      });
+      const data = await Wishlist.create(newWishlist)
+      console.log(data)
+      if (!data) throw err
+      const createdWishlist = await Wishlist.findOne({
+        where: { ProductId, UserId },
+        include: ['Product', 'User'],
+      })
+      
+      res.status(201).json(createdWishlist)
     } catch (err) {
-      next(err);
+      console.log(err, 38)
+      next(err)
     }
   }
 
   static async getWishlist(req, res, next) {
     try {
-      const UserId = req.decoded.id;
+      const UserId = req.decoded.id
 
       const data = await Wishlist.findAll({
         where: { UserId },
         include: ['Product'],
-      });
+      })
 
-      if (!data) throw err;
+      if (!data) throw err
 
-      res.status(200).json(data);
+      res.status(200).json(data)
     } catch (err) {
-      next(err);
+      next(err)
     }
   }
 
   static async deleteWishlist(req, res, next) {
     try {
-      const { id } = req.params;
+      const { id } = req.params
 
       const data = await Wishlist.destroy({
         where: { id },
-      });
+      })
 
-      if (!data) throw err;
+      if (!data) throw err
 
       res.status(200).json({
         msg: 'Wishlist deleted',
-      });
+      })
     } catch (err) {
-      next(err);
+      next(err)
     }
   }
 }
 
-module.exports = WishlistController;
+module.exports = WishlistController
