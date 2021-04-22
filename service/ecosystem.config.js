ae6e399f20386da0fa5661ea737959668bf7f626
J-R4 <JoshuaFanera@gmail.com>
2021-04-22 10:08:59 +0700

    try to deploy (2)

---
+++ service/ecosystem.config.js
@@ -0,0 +1,16 @@
+/** @format */
+
+module.exports = {
+    apps: [
+        {
+            name: 'second. - Server',
+            script: 'npm install && node bin/http.js',
+            env: {
+                PORT: 80,
+                SECRET_KEY: 'secondfinalproject',
+                AccessKeyID: 'AKIA3ZVZ5SWUX7SBYU2U',
+                SecretAccessKey: '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',
+            },
+        },
+    ],
+};
